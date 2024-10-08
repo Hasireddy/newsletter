66b0697dd2e28a5f86c8060dcec17f75b45f9814
Hasireddy <malikireddy.uma@gmail.com>
2024-10-08 14:53:13 +0200

    new app created

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appnewsletter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
