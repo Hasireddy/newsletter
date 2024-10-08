b3f44719b44059bd00edf4ba9f01c4fbb278ac0c
Hasireddy <malikireddy.uma@gmail.com>
2024-10-08 15:00:55 +0200

    Model to store user email created

--- appnewsletter/models.py
+++ appnewsletter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class NewsLetterEmail(models.Model):
+    userEmail = models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return self.userEmail
